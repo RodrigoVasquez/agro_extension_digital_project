95bbd82cb864bfd68a3e5579c75e9982ab58beac
Rodrigo Vasquez <rodrigo.vasquez.fernandez@gmail.com>
2025-05-21 16:40:42 -0400

    text2sql using llama_index

---
+++ agents/agent_aa_app/tools.py
@@ -0,0 +1,12 @@
+import os
+from google.adk.tools.application_integration_tool.application_integration_toolset import ApplicationIntegrationToolset
+
+estandar_aa_tool = ApplicationIntegrationToolset(
+    project=os.getenv("GOOGLE_CLOUD_PROJECT"),
+    location=os.getenv("GOOGLE_CLOUD_LOCATION"),
+    connection="c0001-bq-connector",
+    actions=["ExecuteCustomQuery"],
+    entity_operations={"estandar_aa": []},
+    tool_name="estandar_aa_tool",
+    tool_instructions="Útil para consultar rápidamente y de forma interactiva cualquier detalle dentro de un catálogo estructurado de estándares y buenas prácticas, permitiendo a los usuarios obtener respuestas precisas sobre criterios específicos identificados por sus códigos o características categóricas como nivel de importancia, dimensión o tema. Facilita la comprensión de las acciones concretas a implementar, los métodos de verificación necesarios, la puntuación asociada a cada elemento o los recursos de apoyo disponibles, convirtiéndose en una ayuda esencial para quienes necesitan navegar, interpretar o aplicar dicho marco normativo o de certificación de manera eficiente."
+)
