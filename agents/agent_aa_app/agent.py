efb30fdfd4eee8e0e4651c7ffd56b22ebda8e544
Rodrigo Vasquez <rodrigo.vasquez.fernandez@gmail.com>
2025-05-24 11:08:15 -0400

    adding chileprunes_cl datastore and the FAQ given by Panchi

--- agents/agent_aa_app/agent.py
+++ agents/agent_aa_app/agent.py
@@ -10,6 +10,7 @@ from agent_aa_app.tools import estandar_aa_tool
 vertex_search_tool_aa = VertexAiSearchTool(data_store_id=os.getenv("DATASTORE_AA_ID"))
 vertex_search_tool_guides = VertexAiSearchTool(data_store_id=os.getenv("DATASTORE_GUIDES_ID"))
 vertex_search_tool_faq = VertexAiSearchTool(data_store_id=os.getenv("DATASTORE_FAQ_ID"))
+vertex_search_tool_chileprunes_cl= VertexAiSearchTool(data_store_id=os.getenv("DATASTORE_CHILEPRUNES_CL_ID"))
 
 aa_agent_rag = LlmAgent(
    name="aa_agent_rag",
@@ -18,7 +19,8 @@ aa_agent_rag = LlmAgent(
    instruction=ESTANDAR_AA_STRUCTURED_SYSTEM_PROMPT,
    tools=[vertex_search_tool_aa,
           vertex_search_tool_guides,
-          vertex_search_tool_faq],
+          vertex_search_tool_faq,
+          vertex_search_tool_chileprunes_cl],
 )
 
 aa_agent_bq = LlmAgent(
@@ -32,8 +34,8 @@ aa_agent_bq = LlmAgent(
 root_agent = LlmAgent(
     name="aa_agent",
     model="gemini-2.0-flash-001",
-    description="TU funcion es coordinar los agentes y retornar la respuesta al usuario en formato whatapps si es que puedes",
-    instruction="TU funcion es coordinar los agentes y retornar la respuesta al usuario en formato whatapps si es que puedes",
+    description="Tu funcion es coordinar los agentes y retornar la respuesta al usuario en formato whatapps si es que puedes",
+    instruction="Tu funcion es coordinar los agentes y retornar la respuesta al usuario en formato whatapps si es que puedes",
     tools=[
         agent_tool.AgentTool(agent=aa_agent_rag),
         agent_tool.AgentTool(agent=aa_agent_bq)
