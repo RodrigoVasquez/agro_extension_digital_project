3658af735e0bc9e34419ed67649b5124e6dd3479
Roberto Solar <roberto.solar.gallardo@gmail.com>
2025-07-31 22:36:40 +0000

    downgrading models

--- agents/agent_pp_app/agent.py
+++ agents/agent_pp_app/agent.py
@@ -13,7 +13,7 @@ vertex_search_tool_chileprunes_cl= VertexAiSearchTool(data_store_id=os.getenv("D
 
 pp_agent_rag = LlmAgent(
    name="pp_agent_rag",
-   model="gemini-2.5-flash",
+   model="gemini-2.5-flash-lite",
    instruction=agent_pp_rag_instruction(),
    description=agent_pp_rag_description(),
    tools=[vertex_search_tool_pp,
@@ -24,7 +24,7 @@ pp_agent_rag = LlmAgent(
 
 pp_agent_bq = LlmAgent(
     name="pp_agent_bq",
-    model="gemini-2.5-flash",
+    model="gemini-2.5-flash-lite",
     instruction=agent_pp_bq_instruction(),
     description=agent_pp_bq_description(),
     tools=[estandar_pp_tool],
@@ -32,7 +32,7 @@ pp_agent_bq = LlmAgent(
 
 root_agent = LlmAgent(
     name="pp_agent",
-    model="gemini-2.5-pro",
+    model="gemini-2.5-flash",
     instruction=agent_pp_instruction(),
     tools=[
         agent_tool.AgentTool(agent=pp_agent_rag),
